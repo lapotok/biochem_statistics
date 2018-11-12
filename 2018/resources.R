69636755a536e15083c6342b249edec2b1216452
lapotok <lapotok@gmail.com>
2018-11-12 11:08:28 +0300

    Update resources.R

--- 2018/resources.R
+++ 2018/resources.R
@@ -3,6 +3,10 @@
 list.of.packages <- c('ggplot2', 'scales', 'plotly', 'openxlsx', 'httr', 'rvest', 'ggpubr', 'lattice', 'reshape2', 'repr', 'car', 'sinaplot', 'cowplot', 'dplyr', 'curl', 'dunn.test', 'ggforce', 'gridExtra', 'knitr', 'multcomp', 'drc', 'RColorBrewer', 'rmarkdown', 'boot', 'nlme', 'caret', 'ipred')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+if (!("Biobase" %in% installed.packages()[,"Package"]) BiocManager::install("Biobase", version = "3.8")
+
 if (length(new.packages) == 0) {
   message('No packages need to be installed.')
 } else {
