030670d3d6bdcb92ea2eaf31d47078ae07ee9102
lapotok <lapotok@gmail.com>
2018-12-02 20:23:01 +0300

    Update resources.R

--- 2018/resources.R
+++ 2018/resources.R
@@ -1,6 +1,7 @@
 # Скачиваем нужные пакеты, если еще не скачаны
 
-list.of.packages <- c('ggplot2', 'scales', 'plotly', 'openxlsx', 'httr', 'rvest', 'ggpubr', 'lattice', 'reshape2', 'repr', 'car', 'sinaplot', 'cowplot', 'dplyr', 'curl', 'dunn.test', 'ggforce', 'gridExtra', 'knitr', 'multcomp', 'drc', 'RColorBrewer', 'rmarkdown', 'boot', 'nlme', 'caret', 'ipred', 'e1071', 'readxl', 'naniar', 'effsize', 'dunn.test', 'magrittr')
+update.packages()
+list.of.packages <- c('ggplot2', 'scales', 'plotly', 'openxlsx', 'httr', 'rvest', 'ggpubr', 'lattice', 'reshape2', 'repr', 'car', 'sinaplot', 'cowplot', 'dplyr', 'curl', 'dunn.test', 'ggforce', 'gridExtra', 'knitr', 'multcomp', 'drc', 'RColorBrewer', 'rmarkdown', 'boot', 'nlme', 'caret', 'ipred', 'e1071', 'readxl', 'naniar', 'effsize', 'dunn.test', 'magrittr', 'officer')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
@@ -11,7 +12,7 @@ if (length(new.packages) == 0) {
   message('No packages need to be installed.')
 } else {
   message(paste(length(new.packages), 'packages need to be installed.'))
-  install.packages(new.packages)
+  install.packages(new.packages, dependencies = TRUE)
 }
 
 # if(!require(devtools)) install.packages("devtools")
