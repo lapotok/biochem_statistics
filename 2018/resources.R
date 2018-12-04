c906f576894b2f4773f905bf2f1376b58e6bec18
lapotok <lapotok@gmail.com>
2018-12-04 12:04:44 +0300

    Update resources.R

--- 2018/resources.R
+++ 2018/resources.R
@@ -1,27 +1,22 @@
 # Скачиваем нужные пакеты, если еще не скачаны
 
 # update.packages()
-list.of.packages <- c('ggplot2', 'scales', 'plotly', 'openxlsx', 'httr', 'rvest', 'ggpubr', 'lattice', 'reshape2', 'repr', 'car', 'sinaplot', 'cowplot', 'dplyr', 'curl', 'ggforce', 'gridExtra', 'knitr', 'multcomp', 'drc', 'RColorBrewer', 'rmarkdown', 'boot', 'nlme', 'caret', 'ipred', 'e1071', 'readxl', 'naniar', 'effsize', 'dunn.test', 'magrittr', 'officer', 'rvg')
+list.of.packages <- c('ggplot2', 'scales', 'plotly', 'openxlsx', 'httr', 'rvest', 'ggpubr', 'lattice', 'reshape2', 'repr', 'car', 'sinaplot', 'cowplot', 'dplyr', 'curl', 'ggforce', 'gridExtra', 'knitr', 'multcomp', 'drc', 'RColorBrewer', 'rmarkdown', 'boot', 'nlme', 'caret', 'ipred', 'e1071', 'readxl', 'naniar', 'effsize', 'dunn.test', 'magrittr', 'officer', 'rvg', 'profvis', 'lubridate', 'remedy', 'glue', 'patchwork', 'esquisse')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-if (!("Biobase" %in% installed.packages()[,"Package"])) BiocManager::install("Biobase", version = "3.8")
 
 if (length(new.packages) == 0) {
   message('No packages need to be installed.')
 } else {
-  message(paste(length(new.packages), 'packages need to be installed.'))
-  install.packages(new.packages, dependencies = TRUE)
+  message(paste('Following packages (', length(new.packages), ') need to be installed:', sep=""))
+  for (p in new.packages) message(paste('*', p))
+  #install.packages(new.packages, dependencies = TRUE)
 }
 
 installed = as.character(installed.packages()[,"Package"])
-if(length(list.of.packages) > length(intersect(list.of.packages, installed))) {
-  message('Following packages are still missing:')
-  setdiff(list.of.packages, installed)
+diff = setdiff(list.of.packages, installed)
+if(length(diff)>0) {
+  message(paste('Following packages (', length(new.packages), ') are still missing:', sep=""))
+  for (p in diff) message(paste('*',p))
+} else {
+  message("All packages were successfully installed!")
 }
-
-
-
-# if(!require(devtools)) install.packages("devtools")
-# devtools::install_github("kassambara/ggpubr")
